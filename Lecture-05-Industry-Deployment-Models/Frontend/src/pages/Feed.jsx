b12e1ec0f351b85ee2d3c848adda56bd1db6cc23
Deepak kumar <dkumar39223@gmail.com>
2026-02-16 10:28:58 +0530

    frontend-done

---
+++ Lecture-05-Industry-Deployment-Models/Frontend/src/pages/Feed.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+
+const Feed = () => {
+    const [posts, setposts] = useState([
+       {
+         _id:"1",
+        image:"https://images.unsplash.com/photo-1770566258012-7d809f99c574?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D",
+        caption:"beautiful nature",
+       }
+       
+      
+    ])
+
+  
+  return (
+    <section className='feed-section' >
+        <h1>Feed</h1>
+        {
+            posts.length>0?(
+                posts.map((post)=>(
+                    <div key={post._id}>
+                        <img src={post.image} alt="" />
+                        <p>{post.caption}</p>
+                    </div>
+                ))
+            ):(
+                <p>No posts yet</p>
+            )
+        }
+
+    </section>
+  )
+}
+
+export default Feed
